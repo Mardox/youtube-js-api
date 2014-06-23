733288c0d0cd68129c8972c9126b12cadd3851ef
HooMan <me@hoomanmardokhi.com>
2014-06-23 18:39:37 +1000

    Update youtube-api.js

--- youtube-api.js
+++ youtube-api.js
@@ -5,7 +5,7 @@
 // 22/10/2013
 // ******************************************/ 
 //  Usage:
-//  var myYouTube = new Youtube(query, query_type, duration, startindex, max_results);
+//  var myYouTube = new Youtube(query, query_type, duration, start_index, max_results);
 //  myYouTube.getVideos(callback);   
 //  
 //  *****************************************/
@@ -13,29 +13,28 @@
 
 
 
-    var Youtube = function (query, query_type, duration, startindex, max_results){
+    var Youtube = function (query, query_type, duration, start_index, max_results){
         this.query = query;
         this.query_type = query_type;
         this.duration = duration;
-        this.startindex = startindex;
+        this.start_index = startindex;
         this.max_results = max_results;
     };
 
     Youtube.prototype.getVideos = function(callback){
 
         var url_duration ="";
-        var url_startindex = "";
+        var url_start_index = "";
         var url_hd ="";
         var api_url ="";
        
         //You tube & building the list view
 
-        console.log('startindex : '+this.startindex);
         if(this.duration){ url_duration ="&duration="+this.duration; }
-        if(this.startindex){ 
-            url_startindex = "&start-index="+this.startindex;
+        if(this.start_index){ 
+            url_start_index = "&start-index="+this.start_index;
             //next itteration start index
-            this.startindex = this.startindex + this.max_results; 
+            this.start_index = this.start_index + this.max_results; 
         }
         if(high_def){ url_hd = "&hd"; }
         if(this.max_results){ url_max_results = "&max-results="+this.max_results }else{ url_max_results = "&max-results=50";}
@@ -48,12 +47,12 @@
         }else if(this.query_type == 'search'){
             //search query
             api_url = 'https://gdata.youtube.com/feeds/api/videos?q='
-            +this.query+'&orderby=relevance'+url_max_results+'&alt=jsonc&v=2'+url_duration+url_startindex+url_hd;
+            +this.query+'&orderby=relevance'+url_max_results+'&alt=jsonc&v=2'+url_duration+url_start_index+url_hd;
 
         }else if(this.query_type == 'playlist'){
             //Playlist query
             api_url = 'https://gdata.youtube.com/feeds/api/playlists/'
-            +this.query+'?alt=jsonc&v=2'+url_max_results+url_duration+url_startindex;
+            +this.query+'?alt=jsonc&v=2'+url_max_results+url_duration+url_start_index;
 
         }
         var api_url_encoded = encodeURI(api_url);
